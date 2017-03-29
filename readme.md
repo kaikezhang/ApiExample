23957e73fb4ea36d96f2e75423702669db8a8b75
kaikezhang <kaikezhang@gmail.com>
2017-03-29 15:30:16 -0400

    Works

--- readme.md
+++ readme.md
@@ -1,1 +1,31 @@
 laravel new ApiExample
+
+create database
+
+modify .env file
+
+php artisan make:model Task --migration
+
+Edit migration file
+
+php artisan migrate
+
+Edite routes/api.php
+
+Test with postman:
+
+Create a task
+
+Post $domain/api/tasks insert name field in body.
+
+Show all tasks
+
+Get $domain/api/tasks
+
+Delete a task
+
+Delete $domain/api/tasks/{id}
+
+Update a task
+
+Put $domain/api/tasks/{id} insert name field in body and select x-www-form-urlencoded.
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Task;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,4 +15,34 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('tasks', function(){
+    return Task::all();
+});
+
+Route::post('tasks', function(Request $request){
+  if($request->has('name')){
+    $name = $request->get('name');
+    $task = new Task;
+    $task->name = $name;
+    $task->save();
+    return response($task->id, 201);
+  }
+  return response('Task must have a name.', 400);
+});
+
+Route::delete('tasks/{task}', function(Task $task){
+    if($task->delete())
+      return response('', 200);
+    return response('', 400);
+});
+
+Route::put('tasks/{task}', function(Request $request, Task $task){
+    if($request->has('name')){
+      $task->name = $request->get('name');
+      $task->save();
+      return response('', 200);
+    }
+    return response('Name field is missing.', 400);
 });
